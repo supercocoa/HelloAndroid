e2a38bfd6beb2f7e0e2c5eb6eb9f56f9944b1cf6
supercocoa <g765462596@gmail.com>
2015-09-13 23:41:23 +0800

    fix fresco init

--- app/src/main/java/com/helloandroid/image/FrescoActivity.java
+++ app/src/main/java/com/helloandroid/image/FrescoActivity.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
+import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 /**
@@ -14,6 +15,7 @@ public class FrescoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
         SimpleDraweeView simpleDraweeView = new SimpleDraweeView(this);
         simpleDraweeView.setLayoutParams(new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
