c73ff67e0f05b3396562969fa5128c0ac38b1013
supercocoa <g765462596@gmail.com>
2015-10-18 23:53:30 +0800

    add rxandroid map reduce test

--- app/src/main/java/com/helloandroid/framework/RxAndroidActivity.java
+++ app/src/main/java/com/helloandroid/framework/RxAndroidActivity.java
@@ -3,22 +3,70 @@ package com.helloandroid.framework;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.helloandroid.app.R;
 import rx.Observable;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.functions.Func2;
 import rx.schedulers.Schedulers;
 
 /**
  * Created by scott on 15/10/17.
  */
 public class RxAndroidActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rxandroid);
+        ButterKnife.bind(this);
+    }
 
+    @OnClick(R.id.btn1)
+    void testJust() {
         Observable.just("1", "2", "3")
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onCompleted() {
+                        Toast.makeText(getApplicationContext(), "finish", Toast.LENGTH_SHORT)
+                                .show();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+    }
+
+    @OnClick(R.id.btn2)
+    void testMapReduce() {
+        String[] contents = new String[]{"Hello", "RxAndroid"};
+
+        Observable.from(contents)
+                .map(new Func1<String, String>() {
+                    @Override
+                    public String call(String content) {
+                        return content.toLowerCase();
+                    }
+                })
+                .reduce(new Func2<String, String, String>() {
+                    @Override
+                    public String call(String s, String s2) {
+                        return s + " " + s2;
+                    }
+                })
                 .subscribe(new Observer<String>() {
                     @Override
                     public void onCompleted() {
