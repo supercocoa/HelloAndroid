1c4900116c430141c5e6d647fa8008e97ab293fc
supercocoa <g765462596@gmail.com>
2016-08-31 12:11:07 +0800

    add MainAppliaction

---
+++ app/src/main/java/com/helloandroid/app/MainApplication.java
@@ -0,0 +1,14 @@
+package com.helloandroid.app;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by scott on 16/8/31.
+ */
+
+public class MainApplication extends MultiDexApplication{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
